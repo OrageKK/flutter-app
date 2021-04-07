adfd208081ad8f277acd3b29e61c0bb4ab321eec
huangkunb <huangkunb@enn.cn>
2021-04-07 18:12:26 +0800

    添加滤镜

--- lib/main.dart
+++ lib/main.dart
@@ -71,15 +71,28 @@ class Home extends StatelessWidget {
                 padding: EdgeInsets.zero,
                 children: [
                   UserAccountsDrawerHeader(
-                      accountName: Text(
-                        'Oragekk',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      accountEmail: Text('Oragekk@gmail.com'),
-                      currentAccountPicture: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/18461481?v=4'),
-                      )),
+                    accountName: Text(
+                      'Oragekk',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    accountEmail: Text('Oragekk@gmail.com'),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://avatars.githubusercontent.com/u/18461481?v=4'),
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.yellow[400],
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://t1.hxzdhn.com/uploads/tu/202011/9999/f8bf76bd21.jpg'),
+                            // 填满整个区域
+                            fit: BoxFit.cover,
+                            // 滤镜（ColorFilter.mode 两个参数：颜色，混合模式）
+                            colorFilter: ColorFilter.mode(
+                              Colors.yellow[400].withOpacity(0.6),
+                              BlendMode.srcOver,
+                            ))),
+                  ),
                   ListTile(
                     title: Text(
                       'Messages',
