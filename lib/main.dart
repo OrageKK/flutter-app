7d87865191f70e89fd843c6e688390c9f440e802
huangkunb <huangkunb@enn.cn>
2021-04-07 17:37:45 +0800

    抽屉

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,75 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Welcome to It's me",
-      home: Home(),
-      theme: ThemeData(primaryColor: Color(0xFFfca104)),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Welcome to It's me",
+        home: Home(),
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+            splashColor: Colors.white70));
   }
 }
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      backgroundColor: Color(0xFFF6F6F6),
-      appBar: AppBar(
-        title: Text("因你不同"),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: ListViewDemo(),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add',
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Color(0xFFF6F6F6),
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation',
+              onPressed: () => debugPrint('Navigation button is pressed.'),
+            ),
+            title: Text("因你不同"),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: () => debugPrint('Search button is pressed.'),
+              ),
+            ],
+            centerTitle: true,
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              // 指示器颜色
+              indicatorColor: Colors.black54,
+              // 指示器大小与图标统一
+              indicatorSize: TabBarIndicatorSize.label,
+              // 指示器宽度
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.local_activity),
+                ),
+                Tab(
+                  icon: Icon(Icons.change_history),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_bike),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_activity, size: 128, color: Colors.black12),
+              Icon(Icons.change_history, size: 128, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128, color: Colors.black12),
+            ],
+          ),
+          drawer: Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('This is drawer')],
+            ),
+          ),
+        ));
   }
 }
