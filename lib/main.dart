23412816f53f6dc5200f6151de970ac91ca80126
huangkunb <huangkunb@enn.cn>
2021-04-07 17:47:29 +0800

    抽屉的打开与关闭

--- lib/main.dart
+++ lib/main.dart
@@ -26,59 +26,94 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-          backgroundColor: Color(0xFFF6F6F6),
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Navigation',
-              onPressed: () => debugPrint('Navigation button is pressed.'),
-            ),
-            title: Text("因你不同"),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                tooltip: 'Search',
-                onPressed: () => debugPrint('Search button is pressed.'),
-              ),
-            ],
-            centerTitle: true,
-            elevation: 0.0,
-            bottom: TabBar(
-              unselectedLabelColor: Colors.black38,
-              // 指示器颜色
-              indicatorColor: Colors.black54,
-              // 指示器大小与图标统一
-              indicatorSize: TabBarIndicatorSize.label,
-              // 指示器宽度
-              indicatorWeight: 1.0,
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.local_activity),
-                ),
-                Tab(
-                  icon: Icon(Icons.change_history),
-                ),
-                Tab(
-                  icon: Icon(Icons.directions_bike),
+            backgroundColor: Color(0xFFF6F6F6),
+            appBar: AppBar(
+              title: Text("因你不同"),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: () => debugPrint('Search button is pressed.'),
                 ),
               ],
+              centerTitle: true,
+              elevation: 0.0,
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                // 指示器颜色
+                indicatorColor: Colors.black54,
+                // 指示器大小与图标统一
+                indicatorSize: TabBarIndicatorSize.label,
+                // 指示器宽度
+                indicatorWeight: 1.0,
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(Icons.local_activity),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.change_history),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.directions_bike),
+                  ),
+                ],
+              ),
             ),
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              Icon(Icons.local_activity, size: 128, color: Colors.black12),
-              Icon(Icons.change_history, size: 128, color: Colors.black12),
-              Icon(Icons.directions_bike, size: 128, color: Colors.black12),
-            ],
-          ),
-          drawer: Container(
-            color: Colors.white,
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('This is drawer')],
+            body: TabBarView(
+              children: <Widget>[
+                Icon(Icons.local_activity, size: 128, color: Colors.black12),
+                Icon(Icons.change_history, size: 128, color: Colors.black12),
+                Icon(Icons.directions_bike, size: 128, color: Colors.black12),
+              ],
             ),
-          ),
-        ));
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    child: Text('header'.toUpperCase()),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Messages',
+                      textAlign: TextAlign.right,
+                    ),
+                    trailing: Icon(
+                      Icons.message,
+                      color: Colors.black12,
+                      size: 22.0,
+                    ),
+                    onTap: () => Navigator.pop(context),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Favorite',
+                      textAlign: TextAlign.right,
+                    ),
+                    trailing: Icon(
+                      Icons.favorite,
+                      color: Colors.black12,
+                      size: 22.0,
+                    ),
+                    onTap: () => Navigator.pop(context),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Settings',
+                      textAlign: TextAlign.right,
+                    ),
+                    trailing: Icon(
+                      Icons.settings,
+                      color: Colors.black12,
+                      size: 22.0,
+                    ),
+                    onTap: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+            )));
   }
 }
