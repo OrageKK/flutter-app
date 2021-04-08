0d459dce1b5d6415cd2edbc1ca255a572f3a8cfa
huangkunb <huangkunb@enn.cn>
2021-04-08 10:38:33 +0800

    创建DrawerDemo小部件

---
+++ lib/demo/drawer_demo.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(
+              'Oragekk',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            accountEmail: Text('Oragekk@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/18461481?v=4'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.yellow[400],
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://t1.hxzdhn.com/uploads/tu/202011/9999/f8bf76bd21.jpg'),
+                // 填满整个区域
+                fit: BoxFit.cover,
+                // 滤镜（ColorFilter.mode 两个参数：颜色，混合模式）
+                colorFilter: ColorFilter.mode(
+                  Colors.yellow[400].withOpacity(0.6),
+                  BlendMode.hardLight,
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Messages',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.message,
+              color: Colors.black12,
+              size: 22.0,
+            ),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Favorite',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.favorite,
+              color: Colors.black12,
+              size: 22.0,
+            ),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Settings',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.settings,
+              color: Colors.black12,
+              size: 22.0,
+            ),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
